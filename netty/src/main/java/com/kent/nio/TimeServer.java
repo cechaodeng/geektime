b09b56e77933dc2160981201245369ab86560362
Kent Dent <dengcechao@hotmail.com>
2019-12-30 23:48:08 +0800

    netty nio server

---
+++ netty/src/main/java/com/kent/nio/TimeServer.java
@@ -0,0 +1,12 @@
+package com.kent.nio;
+
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+
+    }
+}
